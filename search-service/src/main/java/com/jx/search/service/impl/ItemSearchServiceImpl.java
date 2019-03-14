d41cc282919c9fc41f0ad4eeb3b03bd38af7e9a5
Raven William <gjnerevmoresf@gmail.com>
2019-03-14 19:10:39 +0800

    实现搜索字段高亮显示的功能

--- search-service/src/main/java/com/jx/search/service/impl/ItemSearchServiceImpl.java
+++ search-service/src/main/java/com/jx/search/service/impl/ItemSearchServiceImpl.java
@@ -1,57 +1,79 @@
 package com.jx.search.service.impl;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.jx.pojo.TbItem;
 import com.jx.search.service.ItemSearchService;
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.Query;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 import com.alibaba.dubbo.config.annotation.Service;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 
-@Service(timeout=5000)
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = "classpath:spring/applicationContext-solr.xml")
+@Service(timeout = 5000)
 public class ItemSearchServiceImpl implements ItemSearchService {
 
-	@Autowired
-	private SolrTemplate solrTemplate;
-	
-	@Override
-	public Map search(Map searchMap) {
-		Map map=new HashMap();
+    @Autowired
+    private SolrTemplate solrTemplate;
 
-		Query query=new SimpleQuery();
-		Criteria criteria=new Criteria("item_keywords").is(searchMap.get("keywords"));
-		query.addCriteria(criteria);
-		
-		ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
-		
-		map.put("rows", page.getContent());
-		
-		return map;
-	}
-
-	@Test
-    public void man() {
-        Map<String,Object> map=new HashMap<>();
-        Query query=new SimpleQuery();
-        //添加查询条件
-        Map searchMap= new HashMap();
-        searchMap.put("keywords","手机");
-        Criteria criteria=new Criteria("item_keywords").is(searchMap.get("keywords"));
+    @Override
+    public Map search(Map searchMap) {
+        Map map = new HashMap();
+/*
+        Query query = new SimpleQuery();
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
+
         ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
-        map.put("rows", page.getContent());
-        System.out.println(map.get("rows"));
+
+        map.put("rows", page.getContent());*/
+
+        //1、查询列表以及高亮显示
+        map.putAll(searchByKeyWordsAndHighLight(searchMap));
+
+        return map;
+    }
+
+    /**
+     * 根据关键字进行搜索，并完成高亮显示
+     *
+     * @param searchMap
+     * @return
+     */
+    private Map searchByKeyWordsAndHighLight(Map searchMap) {
+        Map map = new HashMap();
+
+        HighlightQuery query = new SimpleHighlightQuery();
+        //设置高亮字段
+        HighlightOptions highlightOptions = new HighlightOptions().addField("item_title");
+        //高亮前缀
+        highlightOptions.setSimplePrefix("<em style='color:red'>");
+        //高亮后缀
+        highlightOptions.setSimplePostfix("</em>");
+        //设置高亮选项
+        query.setHighlightOptions(highlightOptions);
+        //按照关键字查询
+        Criteria criteria = new Criteria("item_keywords").is(searchMap.get("keywords"));
+        query.addCriteria(criteria);
+        HighlightPage<TbItem> highlightPage = solrTemplate.queryForHighlightPage(query, TbItem.class);
+        //高亮入口的集合
+        List<HighlightEntry<TbItem>> highlightEntryList = highlightPage.getHighlighted();
+        for (HighlightEntry<TbItem> entry : highlightEntryList) {
+            //获取实体
+            TbItem item = entry.getEntity();
+            //判断是否为空
+            if (entry.getHighlights().size()>0 && entry.getHighlights().get(0).getSnipplets().size()>0){
+                //设置名称高亮
+                item.setTitle(entry.getHighlights().get(0).getSnipplets().get(0));
+            }
+        }
+
+        map.put("rows",highlightPage.getContent());
+        return map;
     }
 }
--- search-web/src/main/webapp/js/base.js
+++ search-web/src/main/webapp/js/base.js
@@ -1,2 +1,9 @@
 // 定义模块:
 var app = angular.module("jx",[]);
+
+//定义过滤器，信任html
+app.filter('trustHtml',["$sce",function ($sce) {
+    return function (data) {
+        return $sce.trustAsHtml(data);
+    }
+}]);
